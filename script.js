554d6ddf49e8e55566d62498e8ecd84a12a79ed4
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2025-10-14 04:19:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,31 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+
 const getSum = () => {
-//Add your code here
+//Add your code 
+
+
+	let pricesall = document.querySelectorAll(".prices");
+
+let total = 0;
+  pricesall.forEach(pricecell => {
+    total += Number(pricecell.innerText);
+  });
+    
+
+let totalrow = document.createElement("tr");
+
+let totalcell = document.createElement("td");
+
+totalcell.colSpan = 2;
+totalcell.innerText = "Total price: Rs" + total;
+
+totalrow.append(totalcell)
+
+const tbody = document.querySelector("table tbody");
+  tbody.append(totalrow);
+
   
 };
 
